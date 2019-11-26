fe969129f1964f3c551b26bec0c0a743e57e3674
Kerem Yilmaz <kerem.yilmaz@udemy.com>
2019-11-26 20:22:03 +0300

    Update main method to return predictions instead of printing

--- src/evaluater/predict.py
+++ src/evaluater/predict.py
@@ -54,11 +54,12 @@ def main(base_model_name, weights_file, image_source, predictions_file, img_form
     for i, sample in enumerate(samples):
         sample['mean_score_prediction'] = calc_mean_score(predictions[i])
 
-    print(json.dumps(samples, indent=2))
+    # print(json.dumps(samples, indent=2))
+    #
+    # if predictions_file is not None:
+    #     save_json(samples, predictions_file)
 
-    if predictions_file is not None:
-        save_json(samples, predictions_file)
-
+    return samples
 
 if __name__ == '__main__':
 
